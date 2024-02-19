d02edba4e72379c104a2f3ced740a2d19d92ade7
saeid tavazani <saeidtavazani@gmail.com>
2024-02-19 03:23:52 -0800

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Container>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
