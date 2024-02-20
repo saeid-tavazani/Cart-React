7735150dcc11b31c3fb381098a0057b83ccb9ba2
saeid tavazani <saeidtavazani@gmail.com>
2024-02-20 04:07:08 -0800

    fix Shopping cart management

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,5 @@
 import { Card, Button } from "react-bootstrap";
+import { useCartContext } from "../context/CartContext";
 type ProductsProps = {
   id: number;
   title: string;
@@ -6,7 +7,8 @@ type ProductsProps = {
   imgUrl: string;
 };
 export default function Products({ id, title, price, imgUrl }: ProductsProps) {
-  const qty: number = 0;
+  const { getItemQty, addItem, decreaseItem, removeItem } = useCartContext();
+  const qty: number = getItemQty(id);
   return (
     <Card>
       <Card.Img
@@ -17,14 +19,14 @@ export default function Products({ id, title, price, imgUrl }: ProductsProps) {
       />
       <Card.Body className="d-flex flex-column bg-dark">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-          <span className="fs-2 text-light">
-            {title.concat(" ").concat(id.toString())}
-          </span>
+          <span className="fs-2 text-light">{title}</span>
           <span className="ms-2 text-light">{price}</span>
         </Card.Title>
         <div className="mt-auto">
           {qty === 0 ? (
-            <Button className="w-100 btn-secondary">Add to Cart</Button>
+            <Button className="w-100 btn-secondary" onClick={() => addItem(id)}>
+              Add to Cart
+            </Button>
           ) : (
             <div
               className="d-flex aling-align-items-center flex-column text-light"
@@ -34,11 +36,25 @@ export default function Products({ id, title, price, imgUrl }: ProductsProps) {
                 className="d-flex align-items-center"
                 style={{ gap: ".5rem" }}
               >
-                <Button className="text-bg-secondary">+</Button>
+                <Button
+                  className="text-bg-secondary"
+                  onClick={() => addItem(id)}
+                >
+                  +
+                </Button>
                 <span className="fs-5 m-3">{qty}</span>
-                <Button className="text-bg-secondary">-</Button>
+                <Button
+                  className="text-bg-secondary"
+                  onClick={() => decreaseItem(id)}
+                >
+                  -
+                </Button>
               </div>
-              <Button className="btn-link-light" size="sm">
+              <Button
+                className="link-dark"
+                size="sm"
+                onClick={() => removeItem(id)}
+              >
                 Remove
               </Button>
             </div>
